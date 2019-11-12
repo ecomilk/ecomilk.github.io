92c464e9db936623e1e4d9205471a37c8367dea0
OdinRossy <remniov.work@gmail.com>
2019-11-12 22:16:30 +0300

    Updates

---
+++ precache-manifest.77d520a8a8d1121a140d18b7b5736535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06c21e397658cb6e005162edfb134a8",
+    "url": "/cheeser/index.html"
+  },
+  {
+    "revision": "419f5cc7f9693296dbbb",
+    "url": "/cheeser/static/css/main.523d6843.chunk.css"
+  },
+  {
+    "revision": "04ee96f55a6d240cea52",
+    "url": "/cheeser/static/js/2.28d2bef2.chunk.js"
+  },
+  {
+    "revision": "419f5cc7f9693296dbbb",
+    "url": "/cheeser/static/js/main.2372b2d7.chunk.js"
+  },
+  {
+    "revision": "984eb3f24298bd9348a5",
+    "url": "/cheeser/static/js/runtime-main.57a6fa88.js"
+  },
+  {
+    "revision": "c7ea3fdb3fdf118cec6f07036d3d9a90",
+    "url": "/cheeser/static/media/header-photo.c7ea3fdb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.a0ea985ecd324541c573ab1628ce661e.js"
+  "/cheeser/precache-manifest.77d520a8a8d1121a140d18b7b5736535.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cheeser/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
