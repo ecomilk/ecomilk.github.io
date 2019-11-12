6fc1ff162f78fabd04f80de0106c22247a454e70
OdinRossy <remniov.work@gmail.com>
2019-11-12 22:14:30 +0300

    Updates

---
+++ precache-manifest.a0ea985ecd324541c573ab1628ce661e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10d23b785a257297041eeae5fd7f61c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9f017cf964c46b911823",
+    "url": "/my-app/static/css/main.9baefd8f.chunk.css"
+  },
+  {
+    "revision": "04ee96f55a6d240cea52",
+    "url": "/my-app/static/js/2.28d2bef2.chunk.js"
+  },
+  {
+    "revision": "9f017cf964c46b911823",
+    "url": "/my-app/static/js/main.2372b2d7.chunk.js"
+  },
+  {
+    "revision": "1e608b227225a98874b1",
+    "url": "/my-app/static/js/runtime-main.42552306.js"
+  },
+  {
+    "revision": "c7ea3fdb3fdf118cec6f07036d3d9a90",
+    "url": "/my-app/static/media/header-photo.c7ea3fdb.png"
+  }
+]);
