df2f824e54ca5c908ca3ebe3aea7a398ecbf8c14
OdinRossy <remniov.work@gmail.com>
2019-12-07 13:51:53 +0300

    minor fix

--- src/components/Product/ProductItem/ProductItem.js
+++ src/components/Product/ProductItem/ProductItem.js
@@ -5,18 +5,20 @@ import './ProductItem.css'
 const ProductItem = (props) => {
 
     const {id, img, title, text} = props;
+    const href = `product/${id}`;
 
     return (
-        <div className="product-item">
+        <div className="product-item"
+            onClick={() => window.open(href)}>
             <div className="card">
                 <img className="card-img-top" src={img} alt=""/>
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text text-secondary">{text}</p>
-                        <div className="text-yellow">
-                            <a href={`product/${id}`}>Подробнее</a>
-                        </div>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text text-secondary">{text}</p>
+                    <div className="text-yellow">
+                        <a href={href}>Подробнее</a>
                     </div>
+                </div>
             </div>
         </div>
     )
