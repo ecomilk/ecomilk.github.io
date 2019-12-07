c9a9228c7e6f89176f1a49800816a0ef14854d72
OdinRossy <remniov.work@gmail.com>
2019-12-07 14:45:10 +0300

    minor fix

--- src/components/Benefit/BenefitsPanel/BenefitsPanel.js
+++ src/components/Benefit/BenefitsPanel/BenefitsPanel.js
@@ -2,42 +2,39 @@ import React from "react";
 import './BenefitsPanel.css'
 import Line from "../../Line";
 import BenefitItem from "../BenefitItem";
+import {Col, Container, Row} from "react-bootstrap";
 
 const BenefitsPanel = (props) => {
 
     const {title, benefitItems = []} = props;
 
     const benefitElements = benefitItems.map(it => (
-        <div
-            key={`benefit-item-container-${it.id}`}
-            className="col-sm">
+        <Col sm={12} md={true} lg={true}
+             key={`benefit-item-container-${it.id}`}>
             <BenefitItem
                 key={`benefit-item-${it.id}`}
                 imgSrc={it.img}
                 title={it.title}
             />
-        </div>
+        </Col>
     ));
 
     return (
-        <div className="row justify-content-md-center pt-sm-5">
-            <div className="col col-md-10 text-center text-md-left text-lg-left">
-                <h2>{title}</h2>
-
-                <div className="row justify-content-start">
-                    <div className="col-sm-3">
-                        <div>
+        <Container>
+            <Row className="justify-content-center pt-sm-5">
+                <Col sm={12} md={true} lg={true} className="text-center text-md-left text-lg-left">
+                    <h2>{title}</h2>
+                    <Row>
+                        <Col sm={10} md={3} lg={3}>
                             <Line/>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row justify-content-sm-around mt-5 mb-5">
-                    {benefitElements}
-                </div>
-            </div>
-        </div>
-
+                        </Col>
+                    </Row>
+                    <Row className="mt-5 mb-5">
+                        {benefitElements}
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
